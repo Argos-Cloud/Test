c135c1f2f0fb40faf6603ca540dd56b97366ed88
Argos <usmc509@hotmail.com>
2019-11-25 11:18:26 -0500

    basic OOP class built, testing the instance of the object and commit

--- Weather_Connector.py
+++ Weather_Connector.py
@@ -1,2 +1,15 @@
 import requests
 import os
+
+
+class Weather(object):
+
+    def __init__(self):
+        self.api_key = "Some Key"
+
+    def print_key(self):
+        print(self.api_key)
+
+
+loc_weather = Weather()
+loc_weather.print_key()
